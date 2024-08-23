dac3669e056d8244fb039e155213e8df1fef6d03
Yuri <yuri.kobets1@gmail.com>
2024-08-23 23:35:28 +0300

    support navigation action policy without app link

--- flutter_inappwebview_platform_interface/lib/src/types/navigation_action_policy.dart
+++ flutter_inappwebview_platform_interface/lib/src/types/navigation_action_policy.dart
@@ -20,4 +20,8 @@ class NavigationActionPolicy_ {
   ///
   ///**NOTE**: available only on iOS 14.5+. It will fallback to [CANCEL].
   static const DOWNLOAD = const NavigationActionPolicy_._internal(2);
+
+  // _WKNavigationActionPolicyAllowWithoutTryingAppLink
+  static const ALLOW_WITHOUT_TRYING_APP_LINK =
+      const NavigationActionPolicy_._internal(3);
 }
